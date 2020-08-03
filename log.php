925d8d51d24ddf472f7d7a439965f436c46eb7ba
orin6205 <68017870+orin6205@users.noreply.github.com>
2020-08-03 12:36:19 +0600

    Add files via upload

---
+++ log.php
@@ -0,0 +1,30 @@
+<body>
+	<table border="1px" border="1" cellpadding="5" cellspacing="0" align="center">
+	<tr>
+	<td colspan="2" width="800px">
+	
+	</td>
+	
+	</tr>
+	<tr heigth="250px">
+	<td>
+	<form action="logcheck.php" method="post">
+	
+	<fieldset width="600px">
+	<legend>LOGIN</legend>
+	<table>
+	<tr>
+	<td>User name</td>
+	<td>:<input type="text" name="name"></td>
+	</tr>
+	<tr>
+	<td>Password</td>
+	<td>:<input type="Password"  name="pass"></td>
+	
+	</tr>
+	</table>
+	<tr>
+	<input type="submit" name="login"><a href="forgotPassword.php">Registration</a>
+	</fieldset>
+	</form>
+	
---
+++ reg.php
@@ -0,0 +1,14 @@
+<fieldset>
+
+ 
+ <legend><b>REGISTRATION</b></legend>
+ <form action="regcheck.php" method="post>
+ ID<br/>
+ 
+ <input type ="ID" name="id"><hr/>
+ Password<br/>
+ <input type="Password" name="password"><br/>
+ Confirm Password<br/>
+ <input type="Password" name="confirm"><br/>
+ NAME<br/>
+ <input type="name" name="uname"><br/>
